762c6dd2d6a639a351a74bb9079ae4c14dd689c7
clareswensen <clareswensen.seattlecolleges.com>
2022-06-07 12:13:06 -0700

    models

--- school/models.py
+++ school/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Course(models.Model):
+    courseTitle = models.CharField(max_length=255)
+    instructor = models.CharField(max_length=255)
+    courseDate = models.DateField()
+    courseTime = models.TimeField()
+    roomNumber = models.IntegerField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.courseTitle
+
+    class Meta:
+        db_table='course'
+        verbose_name_plural='courses'
+
+class CourseAssignment(models.Model):
+    courseID = models.ForeignKey(Course, on_delete=models.DO_NOTHING)
+    assignmentName = models.CharField(max_length=255)
+    dueDate = models.DateField()
+    assignmentDetails = models.TextField()
+    
+
+    def __str__(self):
+        return self.assignmentName
+
+    class Meta:
+        db_table='courseAssignment'
+        verbose_name_plural='coursesAssignments'
--- schooltracker/settings.py
+++ schooltracker/settings.py
@@ -78,8 +78,8 @@ WSGI_APPLICATION = 'schooltracker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
+        'NAME':config('DB_NAME'),
+        'USER':config('DB_USER'),
         'PASSWORD':config('DB_PASSWORD'),
         'HOST':config('DB_HOST'),
         'PORT':config('DB_PORT'),
